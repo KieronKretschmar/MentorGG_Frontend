a3980edd55cc811aba1fe015d185b71988b94f9f
Felix Lehmann <buffygr@web.de>
2020-02-13 17:27:30 +0100

    Added InputBlock during app init

---
+++ src/components/InputBlock.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="input-block">
+        <img src="@/assets/logo_white.svg" />
+        <AjaxLoader><slot/></AjaxLoader>
+    </div>
+</template>
+
+<style lang="scss">
+.input-block {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    background: $dark-1;
+    z-index: 99999;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        max-width: 400px;
+        margin-bottom: 30px;
+        padding-bottom: 30px;
+        border-bottom: 1px solid $purple;
+    }
+}
+</style>
