ec5513661ca32342e9e77d53b10f47b0ed5d08bf
Kieron <kieron@mentor.gg>
2020-02-14 12:54:50 +0100

    Add Source enum

--- src/enums.js
+++ src/enums.js
@@ -1,47 +1,59 @@
-class Enums {  
+class Enums {
   static get SampleType() {
     return {
-        Molotov: 1,
-        Flash: 2,
-        HE: 3,
-        Smoke: 4,
-        Kill: 5,
-        Bomb: 6,
+      Molotov: 1,
+      Flash: 2,
+      HE: 3,
+      Smoke: 4,
+      Kill: 5,
+      Bomb: 6,
     };
   }
 
   static get RadarViewTypes() {
     return {
-        Sample: 1,
-        Zone: 2,
-        Lineup: 3,
+      Sample: 1,
+      Zone: 2,
+      Lineup: 3,
     };
   }
 
   static get SubscriptionStatus() {
     return {
-        Free: 1,
-        Premium: 2,
-        Ultimate: 3,
+      Free: 1,
+      Premium: 2,
+      Ultimate: 3,
 
-        ToString(n) {
-          switch (n) {
-            case 1: return "Free";
-            case 2: return "Premium";
-            case 3: return "Ultimate";
-          }
-
-          return "Unknown";
+      ToString(n) {
+        switch (n) {
+          case 1: return "Free";
+          case 2: return "Premium";
+          case 3: return "Ultimate";
         }
+
+        return "Unknown";
+      }
     };
   }
 
-
   static get DemoViewerQuality() {
     return {
-        Low: 1,
-        Medium: 2,
-        High: 3,
+      Low: 1,
+      Medium: 2,
+      High: 3,
+    };
+  }
+
+  static get Source() {
+    return {
+      Unknown: 0,
+      Valve: 1,
+      Faceit: 2,
+      ManualUpload: 3,
+      Scrimmage: 4,
+      HLTV: 5,
+      Esea: 6,
+      Cevo: 7,
     };
   }
 }
