3316f16fdccfd4f1fba559fa9703c29c1350a2d9
Linus <linusbiela@outlook.de>
2020-02-27 13:46:29 +0100

    Add responsive style

--- src/components/ProfileHeader.vue
+++ src/components/ProfileHeader.vue
@@ -107,43 +107,45 @@ export default {
 @media (max-width: 800px) {
   .profile-header {
     background: $dark-3;
-    padding: 40px 0px;
+    padding: 20px 0 0 0;
     border-bottom: 1px solid $purple;
-    padding-bottom: 110px;
+    padding-bottom: 90px;
 
     .fixed-width-container {
       display: flex;
-      align-items: center;
-      flex-wrap: wrap;
+      align-items: flex-start;
+      height: 60px;
 
       .avatar {
         border-radius: 5px;
         overflow: hidden;
 
         img {
-          width: 110px;
-          height: 110px;
+          max-width: 60px;
+          height: 60px;
         }
       }
 
       .name {
         display: flex;
         flex-direction: column;
-        margin-left: 25px;
+        margin-left: 10px;
         padding-right: 10px;
         border-right: none;
+        height: 60px;
+        max-width: 125px;
 
         span {
-          margin: 5px 0;
+          margin: 5px;
 
           &:first-child {
             color: $orange;
-            font-size: 12px;
+            font-size: 10px;
           }
 
           &:last-child {
             color: white;
-            font-size: 1.75rem;
+            font-size: 12px;
             font-weight: 700;
           }
         }
@@ -152,20 +154,24 @@ export default {
       .csgo-rank {
         display: flex;
         flex-direction: column;
-        margin-left: 25px;
+        margin-left: 0;
+        padding-left: 10px;
         text-align: center;
+        border-left: 1px solid $purple;
+        max-width: 125px;
+        height: 60px;
 
         span {
           margin: 5px 0;
 
           &:first-child {
             color: $orange;
-            font-size: 12px;
+            font-size: 10px;
           }
         }
 
         img {
-          height: 44px;
+          height: 30px;
           margin: 5px 0;
         }
       }
