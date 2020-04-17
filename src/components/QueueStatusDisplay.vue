e06e8fd25f58ef3a0a25c27fd19a3a64c228f7c9
Linus <linusbiela@outlook.de>
2020-04-17 13:59:02 +0200

    Bugfix delete span

--- src/components/QueueStatusDisplay.vue
+++ src/components/QueueStatusDisplay.vue
@@ -20,7 +20,7 @@
           </div>
         </div>
 
-        <div class="demo-text-bold scrolling-text" v-if="dailyLimitReached === !true">DAILY LIMIT REACHED UNTIL <span class="orange-bold">{{ match.MatchDate|formatDateAndTime }}</span></div>
+        <div class="demo-text-bold scrolling-text" v-if="dailyLimitReached === !true">DAILY LIMIT REACHED UNTIL</div>
         
       </div>
       
