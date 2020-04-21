93b4b92c6572a75491062e48f495c7528ef482b1
Arran <code@arran.nz>
2020-04-21 14:20:11 +0200

    Format TeamToggle

--- src/components/RadarImageFeatures/TeamToggle.vue
+++ src/components/RadarImageFeatures/TeamToggle.vue
@@ -1,37 +1,23 @@
 <template>
-    <label class="switch-wrap team-toggle" @click="SetShowCt(!isCt)">
-
-        <div class="switch" 
-        v-bind:class="{ right: isCt, left:!isCt}">        
-            <img :src="require(`@/assets/${teamImage}`)" />
-        </div>
-    
-    </label>
+  <label class="switch-wrap team-toggle" @click="SetShowCt(!isCt)">
+    <div class="switch" :class="{ right: isCt, left:!isCt}">
+      <img :src="require(`@/assets/${teamImage}`)" />
+    </div>
+  </label>
 </template>
 
 <script>
 export default {
-    props: [
-    "isCt",
-    "SetShowCt",
-    ],
-    computed: {
-        teamImage: {
-            get: function(){
-                if (this.isCt){
-                    return "ct_logo.png"
-                }
-                else{
-                    return "t_logo.png"
-                }
-            }
-        }
+  props: ["isCt", "SetShowCt"],
+  computed: {
+    teamImage() {
+      return this.isCt ? "ct_logo.png" : "t_logo.png";
     }
-}
+  }
+};
 </script>
 
 <style lang="scss">
-
 $iconSize: 28;
 $width: 60px;
 $height: $iconSize;
@@ -39,59 +25,52 @@ $height: $iconSize;
 $ct-glow: #3d78cc;
 $t-glow: #a89966;
 
-
 .switch-wrap {
-    cursor: pointer;
-    
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
+  cursor: pointer;
 
-    width: $width;
-    height: $height;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 
-    background-color: $dark-3;
-    border-radius: 25px;
-    border: 2px solid $purple;
+  width: $width;
+  height: $height;
 
-    user-select: none; 
+  background-color: $dark-3;
+  border-radius: 25px;
+  border: 2px solid $purple;
 
-    // When the wrapper is hovered, apply changed to the switch
-    &:hover div.switch{
+  user-select: none;
+
+  // When the wrapper is hovered, apply changed to the switch
+  &:hover div.switch {
     filter: brightness(1.25);
+  }
+
+  .right {
+    transform: translateX($width - $iconSize - 4px);
+
+    img {
+      filter: drop-shadow(0px 0px 5px $ct-glow);
     }
+  }
 
-
-    .right {
-        transform: translateX($width - $iconSize - 4px);
-
-        img {
-            filter: drop-shadow(0px 0px 5px $ct-glow);
-        }
+  .left {
+    img {
+      filter: drop-shadow(0px 0px 5px $t-glow);
     }
+  }
 
-    .left {
-        img{
-            filter: drop-shadow(0px 0px 5px $t-glow);
-        }
+  .switch {
+    transition: all 300ms cubic-bezier(0.46, 0.33, 0, 1.4);
+    vertical-align: middle;
+
+    height: unquote($iconSize + "px");
+    width: unquote($iconSize + "px");
+
+    img {
+      width: 100%;
+      height: 100%;
     }
-
-    .switch {
-
-        transition: all 300ms cubic-bezier(.46,.33,0,1.4);
-        vertical-align: middle;
-
-        height: unquote($iconSize + "px");
-        width: unquote($iconSize + "px");
-        
-        img {
-            width: 100%;
-            height: 100%;
-        }
-    }
-
+  }
 }
-
-
-
 </style>
