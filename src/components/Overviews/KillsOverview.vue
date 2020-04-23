4dd8b7b436994d10a355575b7ae11315bfd38e6f
Linus <linusbiela@outlook.de>
2020-04-23 13:48:24 +0200

    Add flex inline div to each section

--- src/components/Overviews/KillsOverview.vue
+++ src/components/Overviews/KillsOverview.vue
@@ -4,46 +4,48 @@
       <AjaxLoader>Computing summaries for each map</AjaxLoader>
     </div>
     <div class="performances">
-      <div
-        v-for="(mapSummary,map) in mapSummaries"
-        :key="map"
-        class="performance"
-        :class="{active: activeMap == map}"
-        @click="OnActiveMapUpdated(map)"
-      >
-        <img class="map-image" :src="$assetLoader.getMapPreview(map)" />
-        <p class="map-name">{{map}}</p>
+      <div class="performance-inline">
+        <div
+          v-for="(mapSummary,map) in mapSummaries"
+          :key="map"
+          class="performance"
+          :class="{active: activeMap == map}"
+          @click="OnActiveMapUpdated(map)"
+        >
+          <img class="map-image" :src="$assetLoader.getMapPreview(map)" />
+          <p class="map-name">{{map}}</p>
 
-        <div class="z-layer-lo">
-          <!-- TODO: Style -->
-          <span class="split-title">MatchWin Rate</span>
-          <div class="split">
-            <span style="color:white;">{{(mapSummary.MatchWinFraction * 100).toFixed(0) }}%</span>
-          </div>
-        </div>
-
-        <div class="z-layer-hi">
-          <span class="split-title">ROUNDWIN RATE</span>
-          <div class="split">
-            <div class="ct">
-              <img src="@/assets/ct_logo.png" />
-              <span>{{(mapSummary.RoundWinFractionAsCt * 100).toFixed(0) }}%</span>
-            </div>
-            <div class="t">
-              <img src="@/assets/t_logo.png" />
-              <span>{{(mapSummary.RoundWinFractionAsTerrorist * 100).toFixed(0) }}%</span>
+          <div class="z-layer-lo">
+            <!-- TODO: Style -->
+            <span class="split-title">MatchWin Rate</span>
+            <div class="split">
+              <span style="color:white;">{{(mapSummary.MatchWinFraction * 100).toFixed(0) }}%</span>
             </div>
           </div>
 
-          <span class="split-title">KD RATIO</span>
-          <div class="split">
-            <div class="ct">
-              <img src="@/assets/ct_logo.png" />
-              <span>{{(mapSummary.KDAsCt).toFixed(2) }}</span>
+          <div class="z-layer-hi">
+            <span class="split-title">ROUNDWIN RATE</span>
+            <div class="split">
+              <div class="ct">
+                <img src="@/assets/ct_logo.png" />
+                <span>{{(mapSummary.RoundWinFractionAsCt * 100).toFixed(0) }}%</span>
+              </div>
+              <div class="t">
+                <img src="@/assets/t_logo.png" />
+                <span>{{(mapSummary.RoundWinFractionAsTerrorist * 100).toFixed(0) }}%</span>
+              </div>
             </div>
-            <div class="t">
-              <img src="@/assets/t_logo.png" />
-              <span>{{(mapSummary.KDAsTerrorist).toFixed(2) }}</span>
+
+            <span class="split-title">KD RATIO</span>
+            <div class="split">
+              <div class="ct">
+                <img src="@/assets/ct_logo.png" />
+                <span>{{(mapSummary.KDAsCt).toFixed(2) }}</span>
+              </div>
+              <div class="t">
+                <img src="@/assets/t_logo.png" />
+                <span>{{(mapSummary.KDAsTerrorist).toFixed(2) }}</span>
+              </div>
             </div>
           </div>
         </div>
--- src/components/Overviews/MolotovsOverview.vue
+++ src/components/Overviews/MolotovsOverview.vue
@@ -4,52 +4,54 @@
       <AjaxLoader>Computing summaries for each map</AjaxLoader>
     </div>
     <div class="performances">
-      <div
-        v-for="(mapSummary,map) in mapSummaries"
-        :key="map"
-        class="performance"
-        :class="{active: activeMap == map}"
-        @click="OnActiveMapUpdated(map)"
-      >
-        <img class="map-image" :src="$assetLoader.getMapPreview(map)" />
-        <p class="map-name">{{map}}</p>
+      <div class="performance-inline">
+        <div
+          v-for="(mapSummary,map) in mapSummaries"
+          :key="map"
+          class="performance"
+          :class="{active: activeMap == map}"
+          @click="OnActiveMapUpdated(map)"
+        >
+          <img class="map-image" :src="$assetLoader.getMapPreview(map)" />
+          <p class="map-name">{{map}}</p>
 
-        <div class="z-layer-lo">
-          <span class="split-title">UNUSED</span>
-          <div class="split">
-            <div class="ct">
-              <img src="@/assets/ct_logo.png" />
-              <span>{{(Math.max(0,1-mapSummary.UsageRatioAsCt)* 100).toFixed(0) }}%</span>
-            </div>
-            <div class="t">
-              <img src="@/assets/t_logo.png" />
-              <span>{{(Math.max(0,1-mapSummary.UsageRatioAsTerrorist)* 100).toFixed(0) }}%</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="z-layer-hi">
-          <span class="split-title">HIT CHANCE</span>
-          <div class="split">
-            <div class="ct">
-              <img src="@/assets/ct_logo.png" />
-              <span>{{(mapSummary.HitChanceAsCt* 100).toFixed(0) }}%</span>
-            </div>
-            <div class="t">
-              <img src="@/assets/t_logo.png" />
-              <span>{{(mapSummary.HitChanceAsTerrorist* 100).toFixed(0) }}%</span>
+          <div class="z-layer-lo">
+            <span class="split-title">UNUSED</span>
+            <div class="split">
+              <div class="ct">
+                <img src="@/assets/ct_logo.png" />
+                <span>{{(Math.max(0,1-mapSummary.UsageRatioAsCt)* 100).toFixed(0) }}%</span>
+              </div>
+              <div class="t">
+                <img src="@/assets/t_logo.png" />
+                <span>{{(Math.max(0,1-mapSummary.UsageRatioAsTerrorist)* 100).toFixed(0) }}%</span>
+              </div>
             </div>
           </div>
 
-          <span class="split-title">DAMAGE</span>
-          <div class="split">
-            <div class="ct">
-              <img src="@/assets/ct_logo.png" />
-              <span>{{mapSummary.AverageDamageAsCt.toFixed(1)}}</span>
+          <div class="z-layer-hi">
+            <span class="split-title">HIT CHANCE</span>
+            <div class="split">
+              <div class="ct">
+                <img src="@/assets/ct_logo.png" />
+                <span>{{(mapSummary.HitChanceAsCt* 100).toFixed(0) }}%</span>
+              </div>
+              <div class="t">
+                <img src="@/assets/t_logo.png" />
+                <span>{{(mapSummary.HitChanceAsTerrorist* 100).toFixed(0) }}%</span>
+              </div>
             </div>
-            <div class="t">
-              <img src="@/assets/t_logo.png" />
-              <span>{{mapSummary.AverageDamageAsTerrorist.toFixed(1)}}</span>
+
+            <span class="split-title">DAMAGE</span>
+            <div class="split">
+              <div class="ct">
+                <img src="@/assets/ct_logo.png" />
+                <span>{{mapSummary.AverageDamageAsCt.toFixed(1)}}</span>
+              </div>
+              <div class="t">
+                <img src="@/assets/t_logo.png" />
+                <span>{{mapSummary.AverageDamageAsTerrorist.toFixed(1)}}</span>
+              </div>
             </div>
           </div>
         </div>
