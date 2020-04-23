5fce0859a13f265302b35348b811324d8e11bc0a
Arran <code@arran.nz>
2020-04-23 13:57:29 +0200

    Bugfix: Add required file for pre-merged overview-juice

---
+++ src/components/RadarImageFeatures/FeatureBase.vue
@@ -0,0 +1,28 @@
+<template>
+</template>
+
+<style lang="scss">
+
+$trajectory-stroke: 10;
+$trajectory-anim-time: 2500ms;
+
+.trajectory {
+    stroke-width: 3px;
+    fill: none;
+    stroke: white;
+    opacity: 1;
+
+    stroke-dasharray: $trajectory-stroke;
+    stroke-linecap: round;
+    animation: dash $trajectory-anim-time linear;
+    animation-iteration-count: infinite;
+    animation-direction: reverse;
+  }
+
+  @keyframes dash {
+    to {
+      // To keep the infintite animation seam-less `dashoffset` must be 2 * stroke
+      stroke-dashoffset: ($trajectory-stroke * 2);
+    }
+  }
+</style>
