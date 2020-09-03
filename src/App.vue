6216bfad99c8033d2e4dae07c0e7b190b83f06bc
Kieron <kieron@mentor.gg>
2020-09-03 13:03:37 +0200

    Revert "Add long queue announcement"
    This reverts commit 77a551544dc76d960fdcd030e8e76d2395758ced.

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,9 @@
           <i class="fas fa-bars" @click="menuVisible = !menuVisible"></i>
         </header>
         <main>
-            <div class="announcement" v-if="true">
+            <div class="announcement" v-if="false">
               <span class="text">
-                Due to high traffic, the queue of demos that need to be analyzed is very long. Please be patient once you've added your demos to the queue and check again later!
+                Due to server issues, <strong>Automatic Upload for Valve Matchmaking is currently disabled</strong>. Please use the <router-link :to="{name: 'browser-extension'}">Browser Extension</router-link> to upload your matches.
               </span>
             </div>
             <div class="auto-upload-not-configured" v-if="showAutomaticUploadSetupPrompt">
